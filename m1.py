8e4dbdebe4b77c676bbeab37524bb8cdcac52b0d
misbafathi <mdhyder9986@gmail.com>
2024-11-25 03:50:58 +0530

    Add files via upload

---
+++ m1.py
@@ -0,0 +1,26 @@
+def encrypt(text,s):
+    result=""
+    for i in range(len(text)):
+        char=text[i]
+        if(char.isupper()):
+            result+=chr((ord(char)-s+65)%26+65)
+        else:
+            result+=chr((ord(char)-s+97)%26+97)
+    return result
+def dencrypt(text,s):
+    result=""
+    for i in range(len(text)):
+        char=text[i]
+        if(char.isupper()):
+            result+=chr((ord(char)-s-65)%26+65)
+        else:
+            result+=chr((ord(char)-s-97)%26+97)
+    return result
+text="attack"
+s=5
+print("text:"+text)
+print("shift:"+str(s))
+encryptword=encrypt(text,s)
+print("cipher:"+encryptword)
+dencryptword=dencrypt(encryptword,s)
+print("plan text:"+dencryptword)
